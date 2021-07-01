9c255ad8dd9aeca19b92c7ddfff9c02ced027863
William Chong <me@williamchong.cloud>
2021-07-02 01:01:39 +0800

    ✨ Add author list suppport

--- components/IscnRegisterForm.vue
+++ components/IscnRegisterForm.vue
@@ -16,6 +16,19 @@
         >
         <label for="description">description</label>
       </fieldset>
+      <fieldset v-for="(author,index) in authors" :key="index">
+        <input
+          :id="`author_${index}_name`"
+          v-model="author.name"
+        >
+        <label :for="`author_${index}_id`">Author Name</label>
+        <input
+          :id="`author_${index}_url`"
+          v-model="author.url"
+        >
+        <label :for="`author_${index}_url`">Author URL</label>
+      </fieldset>
+      <button @click.prevent="onClickAddAuthor">Add Author</button>
       <fieldset>
         <input
           id="tagsString"
@@ -43,6 +56,11 @@
 <script lang="ts">
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Vue from 'vue';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { mapGetters } from 'vuex';
+import { Author } from '~/types/author';
+
+import { signISCNTx } from '~/utils/cosmos';
 
 export default Vue.extend({
   name: 'IscnRegisterForm',
@@ -52,41 +70,70 @@ export default Vue.extend({
       required: false,
       default: null,
     },
+    fileSHA256: {
+      type: String,
+      required: true,
+    },
     ipfsHash: {
       type: String,
       required: true,
     },
   },
   data(): {
+      authors: Author[];
       title: string;
       description: string;
       tagsString: string;
       url: string;
       license: string;
+      authorName: string;
+      authorUrl: string;
   } {
     return {
+      authors: [],
       title: '',
       description: '',
       tagsString: '',
       url: '',
       license: '',
+      authorName: '',
+      authorUrl: '',
     };
   },
   computed: {
+    ...mapGetters('signer', {
+      address: 'getAddress',
+      signer: 'getSigner',
+    }),
     tags(): string[] {
       return (this.tagsString as string).split(',');
     },
+    authorNames() {
+      return this.authors.map(a => a.name);
+    },
+    authorUrls() {
+      return this.authors.map(a => a.url);
+    },
   },
   methods: {
-    onSubmit() {
+    onClickAddAuthor() {
+      this.authors.push({ name: '', url: '' });
+    },
+    async onSubmit() {
       const payload = {
         title: this.title,
         description: this.description,
         tagsString: this.tagsString,
         url: this.url,
         license: this.license,
+        ipfsHash: this.ipfsHash,
+        fileSHA256: this.fileSHA256,
+        authorNames: this.authorNames,
+        authorUrls: this.authorUrls,
       };
       console.log(payload);
+      const tx = await signISCNTx(payload, this.signer, this.address);
+      console.log(tx);
     },
   }
 })
--- utils/cosmos.ts
+++ utils/cosmos.ts
@@ -1,16 +1,95 @@
-import network from '~/constant/network';
+import {  Registry } from "@cosmjs/proto-signing";
+import {
+  defaultRegistryTypes,
+  assertIsBroadcastTxSuccess,
+  SigningStargateClient,
+} from "@cosmjs/stargate";
+import { DEFAULT_GAS_PRICE, DEFAULT_GAS_PRICE_NUMBER } from "~/constant";
+import { MsgCreateIscnRecord } from "~/constant/codec/iscn/tx";
+import config from "~/constant/network";
 
-export function configToKeplrCoin(denom: string) {
-  const c = network.coinLookup.find(coin => coin.viewDenom === denom);
-  if (!c) return {};
+const registry = new Registry([
+  ...defaultRegistryTypes,
+  ["/likechain.iscn.MsgCreateIscnRecord", MsgCreateIscnRecord], // Replace with your own type URL and Msg class
+]);
+
+function formatISCNPayload(payload, version = 1) {
+  const {
+    title,
+    description,
+    tagsString,
+    url,
+    license,
+    ipfsHash,
+    fileSHA256,
+    type,
+    authorNames,
+    authorUrls,
+  } = payload;
+
+  const contentFingerprints = [];
+  if (fileSHA256) contentFingerprints.push(`hash://sha256/${fileSHA256}`);
+  if (ipfsHash) contentFingerprints.push(`ipfs://${ipfsHash}`);
+  const stakeholders = []; // TODO: parse this
+  if (authorNames.length) {
+    for (let i = 0; i < authorNames.length; i += 1) {
+      const authorName = authorNames[i];
+      const authorUrl = authorUrls[i];
+      const isNonEmpty = authorUrl || authorName;
+      if (isNonEmpty) {
+        stakeholders.push({
+          entity: {
+            id: authorUrl || undefined,
+            name: authorName,
+          },
+          rewardProportion: 1,
+          contributionType: 'http://schema.org/author',
+        })
+      }
+    }
+  }
+  const keywords = tagsString;
+  const usageInfo = license;
   return {
-    coinDenom: c.viewDenom,
-    coinMinimalDenom: c.chainDenom,
-    coinDecimals: c.chainToViewConversionFactor
-      .toString()
-      .split('.')[1].length,
-    coinGeckoId: c.coinGeckoId,
-  };
+    recordNotes: '',
+    contentFingerprints,
+    stakeholders,
+    contentMetadata: {
+      "@context": "http://schema.org/",
+      "@type": "CreativeWorks",
+      title,
+      description,
+      version,
+      url,
+      keywords,
+      usageInfo
+    }
+  }
 }
 
-export default configToKeplrCoin
+export async function signISCNTx(tx, signer, address) {
+  const record = formatISCNPayload(tx);
+  const client = await SigningStargateClient.connectWithSigner(
+    config.apiURL,
+    signer,
+    { registry }
+  );
+
+  const message = {
+    typeUrl: "/likechain.iscn.MsgCreateIscnRecord",
+    value: {
+      from: address,
+      record,
+    },
+  };
+  const fee = {
+    amount: [ DEFAULT_GAS_PRICE ],
+    gas: DEFAULT_GAS_PRICE_NUMBER,
+  };
+  const memo = "Use your power wisely";
+  const response = await client.signAndBroadcast(address, [message], fee, memo);
+  assertIsBroadcastTxSuccess(response);
+  return response;
+}
+
+export default signISCNTx;
