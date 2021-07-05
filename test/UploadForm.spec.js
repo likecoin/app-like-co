d2f887f075a1d37ff81dd646e93cf49fede071d4
William Chong <me@williamchong.cloud>
2021-07-06 01:51:03 +0800

    ✅ Update test template

---
+++ test/UploadForm.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import UploadForm from '@/components/UploadForm.vue'
+
+describe('UploadForm', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(UploadForm)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
