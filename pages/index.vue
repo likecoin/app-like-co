81714d581de33af1e4c57f3c72b155b274d1bc08
William Chong <me@williamchong.cloud>
2021-07-10 09:25:14 +0800

    ✨ Unify iscn data and id parsing from query result

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
       <form @submit.prevent="onSearchISCN">
         <input v-model="searchISCNText" placeholder="Find ISCN by ID">
         <button class="mx-2">Search</button>
+        <div v-if="isNotFound">No ISCN record found</div>
       </form>
     </div>
     <div class="my-4">
@@ -16,29 +17,28 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Vue from 'vue';
 
-import {
-  queryRecordsByTx,
-  queryRecordsById,
-  queryRecordsByFingerprint,
-  queryRecordsByOwner,
-} from '~/utils/cosmos/iscn/query';
+import { queryISCNByAll } from '~/utils/cosmos/iscn/query';
 
 export default Vue.extend({
   data() {
     return {
       searchISCNText: '',
+      isNotFound: false,
     };
   },
   methods: {
     async onSearchISCN() {
+      this.isNotFound = false;
       const { searchISCNText } = this;
-      const res = await Promise.all([
-        queryRecordsByTx(searchISCNText).catch(() => {}),
-        queryRecordsById(searchISCNText).catch(() => {}),
-        queryRecordsByFingerprint(searchISCNText).catch(() => {}),
-        queryRecordsByOwner(searchISCNText).catch(() => {}),
-      ]);
+      const res = await queryISCNByAll(searchISCNText);
       console.log(res);
+      if (!res.length) {
+        this.isNotFound = true;
+      } else if (res.length > 1) {
+        // this.$router.push({ name: 'view', params: { iscnRecord: res }});
+      } else {
+        // this.$router.push({ name: 'search', params: { iscnRecords: res }});
+      }
     }
   },
 })
