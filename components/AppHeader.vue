ba5ce8fe02dad00f89ddae70eb01158678ee8191
William Chong <me@williamchong.cloud>
2021-07-13 04:18:16 +0800

    ✨ Implement my publishing UI

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,7 +3,7 @@
   <nav class="py-8 flex content-center">
     <div class="flex bg-gray-200 mx-auto p-6 rounded-lg divide-white divide-x-2">
       <nuxt-link class="px-2 text-center" :to="{ name: 'index' }">#DePub</nuxt-link>
-      <nuxt-link class="px-2 text-center" :to="{ name: 'index' }">Your publishing</nuxt-link>
+      <nuxt-link class="px-2 text-center" :to="{ name: 'works' }">Your publishing</nuxt-link>
       <a class="px-2 text-center" target="_blank" rel="noopener" href="https://stale.like.co">Delegate and vote</a>
     </div>
     <div class="bg-green-300 absolute right-0 rounded-lg mx-2 my-4 p-2">
