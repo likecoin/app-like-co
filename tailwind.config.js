c4a62fddaf11eb1c2e68b01ce409d2ebf8d3a226
Ng Wing Tat, David <i@ngwingt.at>
2021-07-28 18:49:49 +0800

    💄 [Component] Add TextField

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  mode: 'jit'
+  mode: 'jit',
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: 'white',
+      red: '#E35050',
+      'like-green': '#28646E',
+      'like-cyan': '#50E3C2',
+      'like-cyan-light': '#AAF1E7',
+      'medium-gray': '#4A4A4A',
+      'shade-gray': '#EBEBEB',
+    },
+    height: {
+      '40px': '40px',
+      '44px': '44px',
+    },
+  },
 }
