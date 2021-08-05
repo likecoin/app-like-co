0994c4ccbb219c584edc520e22db9a40c74104f8
Ng Wing Tat, David <i@ngwingt.at>
2021-08-05 15:09:17 +0800

    🔧 Update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,21 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      white: 'white',
+      black: '#000',
+      white: '#FFF',
       red: '#E35050',
       'like-green': '#28646E',
       'like-cyan': '#50E3C2',
       'like-cyan-light': '#AAF1E7',
-      'medium-gray': '#4A4A4A',
+      'dark-gray': '#4A4A4A',
+      'medium-gray': '#9B9B9B',
       'shade-gray': '#EBEBEB',
     },
     height: {
       '40px': '40px',
       '44px': '44px',
+      '48px': '48px',
+      '64px': '64px',
     },
   },
 }
