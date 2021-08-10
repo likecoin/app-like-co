77b41ed6620ef65af67ca42504fe3aef4a1bb3db
AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
2021-08-10 16:30:46 +0800

    💄 [Component] Add FormField
    Also added basic Link and Tag

---
+++ components/Link.vue
@@ -0,0 +1,27 @@
+<template>
+  <nuxt-link
+    :to="to"
+    :class="[
+      'flex',
+      'flex-row',
+      'items-baseline',
+      'text-[16px]',
+      'font-normal',
+      'text-like-green',
+      'leading-[22px]',
+      'underline',
+    ]"
+  >
+    <slot />
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Link extends Vue {
+  // Equivalent to `to` of `<NuxtLink/>`
+  @Prop({ default: null }) readonly to: object | null | undefined
+}
+</script>
