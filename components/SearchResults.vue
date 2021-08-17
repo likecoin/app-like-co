e43c7819d1c1f065ee0f58df79b109b3e0b8c690
AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
2021-08-17 16:50:02 +0800

    💄 [UI] Update SearchResults

--- components/SearchResults.vue
+++ components/SearchResults.vue
@@ -1,22 +1,55 @@
 <template>
-  <div>
+  <div class="container max-w-[976px] flex flex-row flex-wrap justify-center">
     <div v-for="record in records" :key="record.id">
-      <div v-if="!record">
+      <template v-if="!record">
         {{ $t('general.loading') }}
-      </div>
-      <div v-else>
-        <ul>
-          <nuxt-link :to="localeLocation({ name: 'view-iscnId', params: { iscnId: record.id }})">{{ record.id }}</nuxt-link>
-          <li>{{ $t('iscn.meta.content.fingerprints') }} {{ record.data.contentFingerprints }}</li>
-        </ul>
-        <ul>
-          <li>{{ $t('iscn.meta.type') }} {{ record.data.contentMetadata['@type'] }}</li>
-          <li>{{ $t('iscn.meta.title') }} {{ record.data.contentMetadata.title }}</li>
-          <li>{{ $t('iscn.meta.description') }} {{ record.data.contentMetadata.description }}</li>
-          <li>{{ $t('iscn.meta.url') }} {{ record.data.contentMetadata.url }}</li>
-          <li>{{ $t('iscn.meta.keywords') }} {{ record.data.contentMetadata.keywords }}</li>
-        </ul>
-      </div>
+      </template>
+      <nuxt-link
+        v-else
+        :to="
+          localeLocation({
+            name: 'view-iscnId',
+            params: { iscnId: record.id },
+          })"
+      >
+        <div class="container w-min mx-[12px] mb-[48px]">
+          <div
+            class="
+              flex
+              w-[220px]
+              h-[440px]
+              justify-center
+              items-center
+              mb-[16px]
+              rounded-[24px]
+              bg-shade-gray
+              text-white
+            "
+          >
+            ISCN Card
+          </div>
+          <Label
+            class="w-min mb-[16px]"
+            :text="record.data.contentMetadata['@type']"
+            tag="div"
+            preset="p5"
+            valign="middle"
+            content-class="font-semibold whitespace-nowrap text-like-green"
+            prepend-class="text-like-green"
+          >
+            <template #prepend>
+              <ISCNTypeIcon :type="record.data.contentMetadata['@type']" />
+            </template>
+          </Label>
+          <FormField :label="$t('iscn.meta.title')">
+            {{ record.data.contentMetadata.title }}
+          </FormField>
+          <FormField label="">
+            <Tag text="Pill Title" />
+            <Tag text="Pill Title" />
+          </FormField>
+        </div>
+      </nuxt-link>
     </div>
   </div>
 </template>
@@ -24,8 +57,7 @@
 <script lang="ts">
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Vue, Component, Prop } from 'vue-property-decorator'
-
-import { parsedISCNRecord } from '~/utils/cosmos/iscn';
+import { parsedISCNRecord } from '~/utils/cosmos/iscn'
 
 @Component
 export default class SearchResults extends Vue {
