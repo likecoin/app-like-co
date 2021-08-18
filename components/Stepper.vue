cda5b83441001720bacad041cbe9684fb8a9df70
AuroraHuang22 <aurora90376@gmail.com>
2021-08-18 14:15:38 +0800

    💄 Add Stepper

---
+++ components/Stepper.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-row flex-nowrap w-min mb-[8px]">
+    <IconStepDot
+      v-for="(item, index) in 4"
+      :key="item"
+      :color="step === index + 1 ? '#50E3C2' : '#EBEBEB'"
+      class="ml-[16px]"
+    />
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Stepper extends Vue {
+  @Prop(String) readonly step: string | undefined
+}
+</script>
