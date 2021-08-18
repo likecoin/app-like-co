bb383d935a4a493ef36d75657e1e21d0c76b17a9
AuroraHuang22 <aurora90376@gmail.com>
2021-08-18 14:15:38 +0800

    💄 [Component] Add Stepper

--- components/icon/StepDot.vue
+++ components/icon/StepDot.vue
@@ -3,20 +3,9 @@
     width="8"
     height="8"
     viewBox="0 0 8 8"
-    :fill="color"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <circle cx="4" cy="4" r="4" :fill="color" />
+    <circle cx="4" cy="4" r="4" fill="currentColor" />
   </svg>
 </template>
-
-<script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
-
-@Component
-export default class StepDot extends Vue {
-  // Dot color
-  @Prop({ default: 'currentColor' }) readonly color: string | undefined
-}
-</script>
 
