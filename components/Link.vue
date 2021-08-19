e4a8628aba4c6349c0258f74ace19cbcb97371dd
AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
2021-08-19 12:26:02 +0800

    ♻️ Refactor tags & remove placeholders

--- components/Link.vue
+++ components/Link.vue
@@ -1,38 +1,11 @@
 <template>
-  <div>
-    <nuxt-link
-      v-if="to"
-      :to="to"
-      :class="[
-        'flex',
-        'flex-row',
-        'items-baseline',
-        'text-[16px]',
-        'font-normal',
-        'text-like-green',
-        'leading-[22px]',
-        'underline',
-      ]"
-    >
-      <slot />
-    </nuxt-link>
-    <a
-      v-else
-      :href="href"
-      :class="[
-        'flex',
-        'flex-row',
-        'items-baseline',
-        'text-[16px]',
-        'font-normal',
-        'text-like-green',
-        'leading-[22px]',
-        'underline',
-      ]"
-    >
-      <slot />
-    </a>
-  </div>
+  <component
+    :is="tag"
+    :class="rootClasses"
+    :to="to || null"
+  > 
+    <slot />
+  </component>
 </template>
 
 <script lang="ts">
@@ -43,5 +16,25 @@ export default class Link extends Vue {
   // Equivalent to `to` of `<NuxtLink/>`
   @Prop({ default: null }) readonly to: object | null | undefined
   @Prop(String) readonly href: string | undefined
+
+  get tag() {
+    if (this.to) return 'NuxtLink'
+    if (this.$attrs.href) return 'a'
+    return 'a'
+  }
+
+  get rootClasses() {
+    return [
+      'flex',
+      'flex-row',
+      'items-baseline',
+      'text-[16px]',
+      'font-normal',
+      'text-like-green',
+      'leading-[22px]',
+      'underline',
+      this.href || this.to,
+    ]
+  }
 }
 </script>
---
+++ components/SearchResult.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="w-min">
+    <div
+      class="
+        flex
+        w-[220px]
+        h-[440px]
+        justify-center
+        items-center
+        mb-[16px]
+        rounded-[24px]
+        bg-shade-gray
+        text-white
+      "
+    >
+      ISCN Card
+    </div>
+    <Label
+      class="w-min mb-[16px]"
+      :text="metadata['@type']"
+      tag="div"
+      preset="p5"
+      valign="middle"
+      content-class="font-semibold whitespace-nowrap text-like-green"
+      prepend-class="text-like-green"
+    >
+      <template #prepend>
+        <ISCNTypeIcon :type="metadata['@type']" />
+      </template>
+    </Label>
+    <FormField :label="$t('iscn.meta.name')">
+      {{ name }}
+    </FormField>
+    <Tag
+      v-for="item in keywords"
+      :key="item.key"
+      :text="item"
+      class="mr-[8px] mb-[4px]"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class SearchResults extends Vue {
+  @Prop() readonly record!: any
+
+  get metadata() {
+    return this.record.data.contentMetadata
+  }
+
+  get name() {
+    return this.metadata.name || this.metadata.title
+  }
+
+  get keywords(): Array<String> {
+    return this.metadata.keywords ? this.metadata.keywords.split(',') : []
+  }
+}
+</script>
+
