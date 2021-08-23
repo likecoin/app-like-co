e4c0384a685efd4761c5bdf7852ab82ade71f0cf
William Chong <me@williamchong.cloud>
2021-08-23 22:29:53 +0800

    🔧 Add mainnet config and build scripts

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ WD=`cd $(dirname "$0") && pwd -P`
 cd "${WD}"
 
 docker build . -t app-like-co
-docker tag app-like-co:latest us.gcr.io/likecoin-develop/app-like-co:latest
-docker -- push us.gcr.io/likecoin-develop/app-like-co:latest
+docker tag app-like-co:latest us.gcr.io/likecoin-foundation/app-like-co:latest
+docker -- push us.gcr.io/likecoin-foundation/app-like-co:latest
 
 cd "${PWD}"
