7e51a8d0ee13108d0bf5966ea9d34ac2215b7548
AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
2021-09-03 14:01:53 +0800

    ✨ [/new/index] Able to upload multiple types of files

---
+++ components/Previewer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-center w-[138px] max-h-[150px] mr-[16px]">
+    <img v-if="isImage" class="object-contain rounded-[8px]" :src="fileData" />
+    <IconFile v-else class="text-dark-gray" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Previewer extends Vue {
+  @Prop({ default: false }) readonly isImage!: boolean
+  @Prop(String) readonly fileData: string | undefined
+}
+</script>
