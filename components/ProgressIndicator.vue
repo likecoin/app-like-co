5cba20e085ab515561d06514f411cf781f799d1a
Ng Wing Tat, David <i@ngwingt.at>
2021-10-04 11:20:03 +0800

    ♻️ [ProgressIndicator] Refactor metrics calculation

--- components/ProgressIndicator.vue
+++ components/ProgressIndicator.vue
@@ -1,14 +1,18 @@
 <template>
   <svg
-    width="154"
-    height="16"
-    viewBox="0 0 154 16"
+    :width="measurement.width"
+    :height="measurement.height"
+    :viewBox="viewBox"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <defs>
       <clipPath :id="clipPathID">
-        <rect width="154" height="16" rx="8" />
+        <rect
+          :width="measurement.width"
+          :height="measurement.height"
+          :rx="rx"
+        />
       </clipPath>
     </defs>
     <rect
@@ -16,47 +20,23 @@
         'fill-current',
         'text-like-cyan-light',
       ]"
-      width="154"
-      height="16"
-      rx="8"
+      :width="measurement.width"
+      :height="measurement.height"
+      :rx="rx"
     />
     <g :clip-path="`url(#${clipPathID})`">
       <rect v-bind="progressProps">
         <template v-if="isIntermediate">
-          <animate
-            attributeName="width"
-            values="100;40"
-            dur="2s"
-            repeatCount="indefinite"
-          />
-          <animateTransform
-            attributeName="transform"
-            attributeType="XML"
-            type="translate"
-            values="-100 0;170 0"
-            dur="1s"
-            repeatCount="indefinite"
-          />
+          <animate v-bind="animatedProps[0].animate" />
+          <animateTransform v-bind="animatedProps[0].transform"/>
         </template>
       </rect>
       <rect
         v-if="isIntermediate"
         v-bind="progressProps"
       >
-        <animate
-          attributeName="width"
-          values="140;80"
-          dur="4s"
-          repeatCount="indefinite"
-        />
-        <animateTransform
-          attributeName="transform"
-          attributeType="XML"
-          type="translate"
-          values="-140 0;308 0"
-          dur="2s"
-          repeatCount="indefinite"
-        />
+        <animate v-bind="animatedProps[1].animate" />
+        <animateTransform v-bind="animatedProps[1].transform" />
       </rect>
     </g>
   </svg>
@@ -85,6 +65,69 @@ export default class ProgressIndicator extends Vue {
 
   uid = uuidv4()
 
+  // eslint-disable-next-line class-methods-use-this
+  get measurement() {
+    return {
+      width: 154,
+      height: 16,
+      progress: {
+        maxWidth: 100,
+        minWidth: 40,
+        offset: 40,
+      },
+    }
+  }
+
+  get viewBox() {
+    return `0 0 ${this.measurement.width} ${this.measurement.height}`
+  }
+
+  get rx() {
+    return this.measurement.height / 2
+  }
+
+  get animatedProps() {
+    const {
+      width,
+      height,
+      progress,
+    } = this.measurement
+    return [
+      {
+        animate: {
+          attributeName: 'width',
+          values: `${progress.maxWidth};${progress.minWidth}`,
+          dur: '2s',
+          repeatCount: 'indefinite',
+        },
+        transform: {
+          attributeName: 'transform',
+          attributeType: 'XML',
+          type: 'translate',
+          values: `-${progress.maxWidth} 0;${width + height} 0`,
+          dur: '1s',
+          repeatCount: 'indefinite',
+        },
+      },
+      {
+        animate: {
+          attributeName: 'width',
+          values: `${progress.maxWidth + progress.offset };${progress.minWidth + progress.offset}`,
+          dur: '4s',
+          repeatCount: 'indefinite',
+        },
+        transform: {
+          attributeName: 'transform',
+          attributeType: 'XML',
+          type: 'translate',
+          values: `-${progress.maxWidth + progress.offset} 0;${width * 2} 0`,
+          dur: '2s',
+          repeatCount: 'indefinite',
+        },
+      },
+    ]
+  }
+
   get clipPathID() {
     return `progress-indicator-${this.uid}`
   }
@@ -98,10 +141,7 @@ export default class ProgressIndicator extends Vue {
   }
 
   get progressWidth() {
-    if (this.isIntermediate) {
-      return 70
-    }
-    return this.normalizedProgress * 154 + 16
+    return this.normalizedProgress * this.measurement.width + this.measurement.height
   }
 
   get progressProps() {
@@ -110,10 +150,10 @@ export default class ProgressIndicator extends Vue {
         'text-like-cyan',
         'fill-current',
       ],
-      x: -16,
+      x: -this.measurement.height,
       width: this.progressWidth,
-      height: 16,
-      rx: 8,
+      height: this.measurement.height,
+      rx: this.rx,
     }
   }
 }
