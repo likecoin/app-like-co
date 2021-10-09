19a440cf13b85281ca40df8f08a953696f1636b8
William Chong <me@williamchong.cloud>
2021-10-10 02:15:28 +0800

    ✨ Add multiple file support in arweave API

--- server/ipfs/index.ts
+++ server/ipfs/index.ts
@@ -1,5 +1,6 @@
 import { IPFS } from 'ipfs-core-types';
 import { create } from 'ipfs-http-client';
+import { ArweaveFile } from '../arweave/types';
 
 const hash = require('ipfs-only-hash');
 
@@ -15,15 +16,48 @@ const getInstance = (() => {
   };
 })();
 
-export async function uploadToIPFS(fileBlob: Buffer) {
+export async function uploadFileToIPFS(fileBlob: Buffer) {
   const client = getInstance()
   const res = await client.add(fileBlob);
   return res;
 }
 
-export async function getIPFSHash(fileBlob: Buffer) {
-  const ipfsHash = await hash.of(fileBlob);
+export async function uploadFilesToIPFS(files: ArweaveFile[], { onlyHash = false } = {}) {
+  const client = getInstance()
+  const directoryName = '/tmp';
+  const promises = client.addAll(
+    files.map(f => ({
+      content: f.buffer,
+      path: `${directoryName}/${f.key}`,
+    })), { onlyHash },
+  );
+  const results = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const result of promises) {
+    results.push(result);
+  }
+  let entry = results.find(r => r.path === directoryName);
+  if (!entry) {
+    entry = results.find((r => r.path.endsWith('index.html')));
+  }
+  if (!entry) return '';
+  const contentHash = entry.cid.toString();
+  return contentHash;
+}
+
+export async function getFileIPFSHash(file: ArweaveFile) {
+  const ipfsHash = await hash.of(file.buffer);
   return ipfsHash;
+}
+
+export async function getFolderIPFSHash(files: ArweaveFile[]) {
+  const dagHash = await uploadFilesToIPFS(files, { onlyHash: true });
+  return dagHash;
+}
+
+export function getIPFSHash(files: ArweaveFile[]) {
+  if (files.length === 1) return getFileIPFSHash(files[0]);
+  return getFolderIPFSHash(files);
 }
 
 export const IPFS_ENDPOINT = 'https://ipfs.infura.io:5001/api/v0';
