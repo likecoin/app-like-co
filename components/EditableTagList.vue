a4e8231e71f4c29f290741bc6fff40bac70b2994
AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
2021-11-17 12:34:49 +0800

    🚸 Specify character limit for text fields

--- components/EditableTagList.vue
+++ components/EditableTagList.vue
@@ -1,5 +1,12 @@
 <template>
-  <div :class="['flex', 'flex-row', 'flex-wrap', 'items-center']">
+  <div
+    :class="[
+      'flex',
+      'flex-row',
+      'flex-wrap',
+      'items-start',
+    ]"
+  >
     <div
       v-for="(tag, i) of tags"
       :key="tag"
@@ -28,10 +35,12 @@
           'cursor-pointer',
         ]"
         @click="deleteChip(i)"
-      ><IconCloseMini/></span>
+      >
+        <IconCloseMini/>
+      </span>
     </div>
     <button
-      v-if="!shouldShowInput"
+      v-if="!shouldShowInput && isTagsLimitNotExceeded"
       type="button"
       :class="[
         'bg-like-cyan-light',
@@ -49,9 +58,17 @@
     >
       <IconAddMini />
     </button>
-    <form class="relative" @submit.prevent="saveChip">
+    <form
+      v-if="shouldShowInput && isTagsLimitNotExceeded"
+      :class="[
+        'flex',
+        'flex-col',
+        'relative',
+        'mb-[4px]',
+      ]"
+      @submit.prevent="saveChip"
+    >
       <input
-        v-if="shouldShowInput"
         ref="input"
         v-model="currentInput"
         type="text"
@@ -59,7 +76,6 @@
           'absolute',
           'py-[6px]',
           'px-[20px]',
-          'mb-[4px]',
           'text-[12px]',
           { 'text-transparent': currentInput.length },
           'bg-transparent',
@@ -74,31 +90,51 @@
         @blur="blurInput"
       />
       <div
-        v-if="shouldShowInput"
         :class="[
+          'flex',
+          'justify-center',
+          'items-center',
+          'box-border',
           'top-0',
           'left-0',
-          'min-h-[30px]',
+          'h-[30px]',
+          { 'border-[1px] border-red': errorMessage },
           'bg-shade-gray',
           'py-[6px]',
           'px-[20px]',
-          'mb-[4px]',
           'text-[12px]',
           'rounded-[16px]',
         ]"
       >
         {{ currentInput }}
       </div>
+      <span
+        v-if="errorMessage"
+        :class="[
+          'block',
+          'self-end',
+          'text-[red]',
+          'text-[10px]'
+        ]"
+      >
+        {{ errorMessage }}
+      </span>
     </form>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Model, Watch } from 'vue-property-decorator'
+import { Vue, Component, Model, Watch, Prop } from 'vue-property-decorator'
 
 @Component
 export default class EditableTagList extends Vue {
   @Model('change', { type: Array, default: () => [] }) value!: Array<string>
+
+  // Character limit for a tag
+  @Prop({ default: 35 }) readonly charactersLimit!: number | undefined
+
+  // Maximum number of tags
+  @Prop(Number) readonly tagsLimit!: number | undefined
 
   tags: Array<string> = []
   currentInput: string = ''
@@ -107,6 +143,24 @@ export default class EditableTagList extends Vue {
 
   $refs!: {
     input: HTMLInputElement
+  }
+
+  get errorMessage() {
+    if (this.charactersLimit && this.currentInput.length > this.charactersLimit) {
+      return this.$t('IscnRegisterForm.warning.exceeded', {
+        current: this.currentInput.length,
+        limit: this.charactersLimit,
+      })
+    }
+    if (this.tags.includes(this.currentInput)) {
+      return this.$t('IscnRegisterForm.warning.exist')
+    }
+    return undefined
+  }
+
+  get isTagsLimitNotExceeded() {
+    if (this.tagsLimit) return this.tags.length < this.tagsLimit
+    return true
   }
 
   created() {
@@ -126,15 +180,16 @@ export default class EditableTagList extends Vue {
   blurInput() {
     if (this.currentInput) {
       this.saveChip()
-      this.$nuxt.$nextTick(() => this.$refs.input.focus())
-    } else {  
+      if (this.isTagsLimitNotExceeded) {
+        this.$nuxt.$nextTick(() => this.$refs.input.focus())
+      }
+    } else {
       this.shouldShowInput = false
     }
   }
 
   saveChip() {
-    if (!this.currentInput) return
-    if (this.tags.includes(this.currentInput)) return
+    if (!this.currentInput || this.errorMessage) return
     this.tags.push(this.currentInput)
     this.emitChange()
     this.currentInput = ''
