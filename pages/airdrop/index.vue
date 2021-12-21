6712e557fbf33cc8c9d0a177618adba9413bb51a
AuroraHuang22 <aurora90376@gmail.com>
2021-12-21 11:38:37 +0800

    💩 Remove [checker/] page

--- pages/airdrop/index.vue
+++ pages/airdrop/index.vue
@@ -1,10 +1,39 @@
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+<template>
+  <div
+    :class="[
+      'flex',
+      'flex-col',
+      'items-center',
+      'w-full',
+    ]"
+  >
+    <img
+      :class="['my-[32px]', 'w-[300px]']"
+      src="/images/airdrop/title_Checker.png"
+    />
+    <div
+      :class="[
+        'flex',
+        'flex-col',
+        'items-center',
+        'bg-light-gray',
+        'pt-[24px]',
+        'pb-[56px]',
+        'px-[56px]',
+        'w-full',
+      ]"
+    >
+      <Label
+        :class="['font-extrabold', 'text-like-green']"
+        :text="$t('AirDrop.label.Comingsoon')"
+        preset="h2"
+      />
+    </div>
+  </div>
+</template>
 
-@Component({
-  fetch(ctx) {
-    ctx.redirect(302, ctx.localeLocation({ name: 'airdrop-check' })!)
-  },
-})
+<script lang="ts">
+import { Vue } from 'vue-property-decorator'
+
 export default class AirdropPage extends Vue {}
 </script>
