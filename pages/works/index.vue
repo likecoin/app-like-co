a97cc0d4b0fb2659ed02d088832b4f76cf11e18c
Ng Wing Tat, David <i@ngwingt.at>
2022-01-19 12:58:35 +0800

    ✨ [/works] Add pagination UI

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -1,20 +1,96 @@
 <template>
   <Page
-    v-if="!records || !records.length"
+    v-if="!pages || !pages.length"
     class="justify-center"
   >
     <Card>
-      <Label :text="$t(!records ? 'general.loading' : 'WorksPage.empty.label')" />
+      <Label :text="$t(!pages ? 'general.loading' : 'WorksPage.empty.label')" />
     </Card>
   </Page>
   <Page v-else>
-    <SearchResults :records="records" />
+    <nav
+      :class="[
+        'flex',
+        'justify-between',
+        'items-center',
+        'w-full',
+        'max-w-[952px]',
+        'mb-[16px]'
+      ]"
+    >
+      <div
+        :class="[
+          'grid',
+          'grid-flow-col',
+          'gap-[8px]'
+        ]"
+      >
+        <Button
+          class="text-dark-gray"
+          preset="tertiary"
+          size="small"
+          :text="$t('WorksPage.pagination.button.first')"
+          :is-disabled="pageNumber === 0"
+          @click="pageNumber = 0"
+        />
+        <Button
+          class="text-dark-gray"
+          preset="tertiary"
+          size="small"
+          :text="$t('WorksPage.pagination.button.previous')"
+          :is-disabled="pageNumber === 0"
+          @click="previousPage"
+        >
+          <template #prepend>
+            <IconArrowLeft />
+          </template>
+        </Button>
+      </div>
+      <div
+        :class="[
+          'grid',
+          'grid-flow-col',
+          'gap-[8px]'
+        ]"
+      >
+        <Button
+          class="text-dark-gray"
+          preset="tertiary"
+          size="small"
+          :text="$t('WorksPage.pagination.button.next')"
+          :is-disabled="pageNumber >= pages.length - 1"
+          @click="nextPage"
+        >
+          <template #append>
+            <IconArrowRight />
+          </template>
+        </Button>
+        <Button
+          class="text-dark-gray"
+          preset="tertiary"
+          size="small"
+          :text="$t('WorksPage.pagination.button.last')"
+          :is-disabled="pageNumber >= pages.length - 1"
+          @click="pageNumber = pages.length - 1"
+        />
+      </div>
+    </nav>
+    <Transition
+      name="works-grid"
+      mode="out-in"
+    >
+      <SearchResults
+        :key="pages[pageNumber][0].id"
+        :records="pages[pageNumber]"
+      />
+    </Transition>
   </Page>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Watch } from 'vue-property-decorator'
 import { namespace } from 'vuex-class'
+import chunk from 'lodash.chunk'
 import { ISCNRecordWithID } from '~/utils/cosmos/iscn/iscn.type'
 
 const signerModule = namespace('signer')
@@ -24,7 +100,8 @@ const iscnModule = namespace('iscn')
   layout: 'wallet',
 })
 export default class WorksIndexPageextends extends Vue {
-  records: ISCNRecordWithID[] | null = null
+  pages: ISCNRecordWithID[][] | null = null
+  pageNumber = 0
 
   @signerModule.Getter('getAddress') currentAddress!: string
   @iscnModule.Action queryISCNByAddress!: (
@@ -42,10 +119,30 @@ export default class WorksIndexPageextends extends Vue {
 
   async refreshWorks() {
     if (!this.currentAddress) {
-      this.records = []
-      return
+      this.pages = []
+    } else {
+      const records = await this.queryISCNByAddress(this.currentAddress)
+      this.pages = chunk(records, 4)
     }
-    this.records = await this.queryISCNByAddress(this.currentAddress)
+  }
+
+  nextPage() {
+    this.pageNumber = Math.min(this.pageNumber + 1, this.pages?.length || 1 - 1)
+  }
+
+  previousPage() {
+    this.pageNumber = Math.max(this.pageNumber - 1, 0)
   }
 }
 </script>
+
+<style>
+.works-grid-enter-active,
+.works-grid-leave-active {
+  transition: opacity .1s ease-out;
+}
+.works-grid-enter,
+.works-grid-leave-to {
+  opacity: 0;
+}
+</style>
