a524d0d73a581e2a9ce01dc61421a7d17fe68b46
AuroraHuang22 <aurora90376@gmail.com>
2022-01-27 21:27:24 +0800

    🔍️ Update OG meta & P1 UI

--- components/AirdropLogin.vue
+++ components/AirdropLogin.vue
@@ -39,10 +39,7 @@
           'gap-[24px]',
         ]"
       >
-        <li
-          v-for="type in connectWalletTypes"
-          :key="type"
-        >
+        <li v-for="type in connectWalletTypes" :key="type">
           <ConnectWalletButton
             :class="[
               'w-full',
@@ -63,7 +60,7 @@
         'px-[24px]',
       ]"
     >
-      <Label
+      <Button
         :class="[
           'hidden',
           'lg:block',
@@ -71,86 +68,37 @@
           'text-medium-gray',
           'mt-[32px]',
         ]"
-        :text="$t('AirDrop.label.connect.address')"
-        preset="p5"
-      />
-      <Label
+        :text="$t('AirDrop.button.withoutWallet')"
+        :to="localeLocation({ name: 'airdrop-claim' })"
+        preset="plain"
+      >
+        <template #append>
+          <IconArrowRight />
+        </template>
+      </Button>
+      <Button
         :class="[
           'text-dark-gray',
           'mt-[8px]',
           'text-center',
           'lg:hidden',
         ]"
-        :text="$t('AirDrop.guide.connect.wallet')"
-        preset="p5"
+        :text="$t('AirDrop.button.withoutWallet')"
+        :to="localeLocation({ name: 'airdrop-claim' })"
+        preset="plain"
       />
-      <div
-        :class="[
-          'flex',
-          'flex-col',
-          'items-center',
-          'justify-center',
-          'mt-[24px]',
-          'p-[16px]',
-          'w-full',
-          'max-w-[450px]',
-          'sm:flex-row',
-        ]"
-      >
-        <TextField
-          v-model="inputAddress"
-          class="flex-grow"
-          :placeholder="$t('AirDrop.placeholder.address')"
-          :error-message="errorMessage"
-        />
-        <Button
-          :class="[
-            'mt-[16px]',
-            'sm:ml-[16px]',
-            'sm:mt-0',
-          ]"
-          preset="secondary"
-          :text="$t('AirDrop.button.check')"
-          @click="handleAddressInput"
-        >
-          <template #prepend>
-            <IconSearch class="w-[20px]" />
-          </template>
-        </Button>
-      </div>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
-import {
-  CONNECT_WALLET_TYPES,
-  COSMOS_ADDRESS_REGEX,
-  OSMOSIS_ADDRESS_REGEX,
-} from '~/constant'
+import { Vue, Component } from 'vue-property-decorator'
+import { CONNECT_WALLET_TYPES } from '~/constant'
 
 @Component
 export default class AirdropLogin extends Vue {
-  @Prop(Boolean) readonly isAirdropStarted: boolean | undefined
-
-  inputAddress: string = ''
-  errorMessage: string = ''
-
   // eslint-disable-next-line class-methods-use-this
   get connectWalletTypes() {
     return CONNECT_WALLET_TYPES
-  }
-
-  handleAddressInput() {
-    this.errorMessage = ''
-    if (
-      !COSMOS_ADDRESS_REGEX.test(this.inputAddress) &&
-      !OSMOSIS_ADDRESS_REGEX.test(this.inputAddress)
-    ) {
-      this.errorMessage = this.$t('AirDrop.errorMessage.address') as string
-      return;
-    }
-    this.$emit('input', this.inputAddress)
   }
 }
 </script>
