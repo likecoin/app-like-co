2892d3a3050d8b552eccd0246814b65f18405084
WeiJun0827 <weijunhung@outlook.com>
2022-04-26 18:16:47 +0800

    🔧 Set `LIKE_TARGET_ADDRESS` to like address

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ config.IPFS_ENDPOINT = 'https://ipfs.infura.io:5001/api/v0';
 config.REPLICA_IPFS_ENDPOINTS = [];
 config.COSMOS_RPC_ENDPOINT = '';
 config.COSMOS_DENOM = IS_TESTNET ? 'nanoekil' : 'nanolike';
-config.LIKE_TARGET_ADDRESS = 'cosmos1rclg677y2jqt8x4ylj0kjlqjjmnn6w6304rrtc';
+config.LIKE_TARGET_ADDRESS = 'like1rclg677y2jqt8x4ylj0kjlqjjmnn6w63uflpgr';
 
 config.NUMBERS_PROTOCOL_BASE_URL = '';
 config.NUMBERS_PROTOCOL_API_KEY = '';
