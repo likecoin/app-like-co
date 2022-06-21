e1680864b59ba30feda0f32a1efe3dd4b0e44471
AuroraHuang22 <aurora90376@gmail.com>
2022-06-21 13:28:21 +0800

    🐛 Fix keplr detection condition

--- store/wallet.ts
+++ store/wallet.ts
@@ -92,64 +92,63 @@ export default class Wallet extends VuexModule {
 
   @Action
   async initKeplr(): Promise<boolean> {
-    if (this.type === 'keplr') return true;
-    if (!window.keplr) {
-      this.toggleKeplrWarning(true)
+    if (window.keplr && this.type === 'keplr') return true;
       let tries = 0;
       const TRY_COUNT = 3;
-      while (TRY_COUNT > tries) {
+      while (!window.keplr && TRY_COUNT > tries) {
         // eslint-disable-next-line no-await-in-loop
         await timeout(1000);
         tries += 1;
       }
+    if (!window.keplr?.experimentalSuggestChain) {
+      this.toggleKeplrWarning(true);
+      return false
     }
-    if (window.keplr && window.keplr.experimentalSuggestChain) {
-      try {
-        await window.keplr.experimentalSuggestChain({
-          chainId: network.id,
-          chainName: network.name,
-          rpc: network.rpcURL,
-          rest: network.apiURL,
-          stakeCurrency: configToKeplrCoin(network.stakingDenom),
-          walletUrlForStaking: network.stakingWalletURL,
-          bip44: {
-            coinType: 118,
-          },
-          bech32Config: {
-            bech32PrefixAccAddr: network.addressPrefix,
-            bech32PrefixAccPub: `${network.addressPrefix}pub`,
-            bech32PrefixValAddr: `${network.addressPrefix}valoper`,
-            bech32PrefixValPub: `${network.addressPrefix}valoperpub`,
-            bech32PrefixConsAddr: `${network.addressPrefix}valcons`,
-            bech32PrefixConsPub: `${network.addressPrefix}valconspub`,
-          },
-          currencies: network.coinLookup.map(({ viewDenom }) => configToKeplrCoin(viewDenom)),
-          feeCurrencies: network.coinLookup.map(({ viewDenom }) => configToKeplrCoin(viewDenom)),
+    try {
+      await window.keplr.experimentalSuggestChain({
+        chainId: network.id,
+        chainName: network.name,
+        rpc: network.rpcURL,
+        rest: network.apiURL,
+        stakeCurrency: configToKeplrCoin(network.stakingDenom),
+        walletUrlForStaking: network.stakingWalletURL,
+        bip44: {
           coinType: 118,
-          gasPriceStep: {
-            low: 1,
-            average: 10,
-            high: 1000,
-          },
-          features: [
-            'stargate',
-            'ibc-transfer',
-            'no-legacy-stdTx',
-            'ibc-go',
-          ],
-        });
-        await window.keplr.enable(network.id);
+        },
+        bech32Config: {
+          bech32PrefixAccAddr: network.addressPrefix,
+          bech32PrefixAccPub: `${network.addressPrefix}pub`,
+          bech32PrefixValAddr: `${network.addressPrefix}valoper`,
+          bech32PrefixValPub: `${network.addressPrefix}valoperpub`,
+          bech32PrefixConsAddr: `${network.addressPrefix}valcons`,
+          bech32PrefixConsPub: `${network.addressPrefix}valconspub`,
+        },
+        currencies: network.coinLookup.map(({ viewDenom }) => configToKeplrCoin(viewDenom)),
+        feeCurrencies: network.coinLookup.map(({ viewDenom }) => configToKeplrCoin(viewDenom)),
+        coinType: 118,
+        gasPriceStep: {
+          low: 1,
+          average: 10,
+          high: 1000,
+        },
+        features: [
+          'stargate',
+          'ibc-transfer',
+          'no-legacy-stdTx',
+          'ibc-go',
+        ],
+      });
+      await window.keplr.enable(network.id);
 
-        const offlineSigner = window.getOfflineSigner(network.id);
-        const accounts = await offlineSigner.getAccounts();
-        this.context.commit('setType', 'keplr')
-        this.context.commit('setSigner', offlineSigner)
-        this.context.commit('setAccounts', accounts)
-        window.localStorage?.setItem(KEY_CONNECTED_WALLET_TYPE, 'keplr')
-        return true;
-      } catch (error) {
-        console.error(error);
-      }
+      const offlineSigner = window.getOfflineSigner(network.id);
+      const accounts = await offlineSigner.getAccounts();
+      this.context.commit('setType', 'keplr')
+      this.context.commit('setSigner', offlineSigner)
+      this.context.commit('setAccounts', accounts)
+      window.localStorage?.setItem(KEY_CONNECTED_WALLET_TYPE, 'keplr')
+      return true;
+    } catch (error) {
+      console.error(error);
     }
     return false;
   }
