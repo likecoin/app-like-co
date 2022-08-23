3269f3d7b9cef0a2130040757f599458aed88891
hsueh kuan tsai <b123456785678@gmail.com>
2022-08-23 14:50:03 +0800

    ♻️ Remove header

--- server/crawler/index.ts
+++ server/crawler/index.ts
@@ -221,11 +221,6 @@ export default async function getCralwerData(url: string) {
         if(error?.response?.status === 403) {
           const browser = await getBrowserPage();
           const page = await browser.newPage();
-          await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36');
-          await page.setViewport({ width: 1024, height: 768 });
-          await page.setExtraHTTPHeaders({
-            'Accept-Language': 'zh-HK,zh-TW;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6', // bypass Cloudflare
-          });
           await page.goto(encodeURI(url as string), { waitUntil: 'networkidle2' });
           content = await page.content();
           await page.close();
