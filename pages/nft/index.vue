74f6ddefd01aa3208067829bf610bf3265159ce7
Ng Wing Tat, David <i@ngwingt.at>
2022-08-29 11:31:55 +0800

    🚸 Redirect /nft to /nft/url

---
+++ pages/nft/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  fetch({ redirect, query, localeLocation }) {
+    redirect(localeLocation({ name: 'nft-url', query })!);
+  },
+})
+export default class NFTPortalPage extends Vue {}
+</script>
