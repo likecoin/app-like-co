b599778beb401087c1fd1a0154291e916e545e50
William Chong <me@williamchong.cloud>
2022-09-24 01:36:55 +0800

    🚑️ Fix outdated gas price format

--- store/wallet.ts
+++ store/wallet.ts
@@ -148,13 +148,15 @@ export default class Wallet extends VuexModule {
           bech32PrefixConsPub: `${network.addressPrefix}valconspub`,
         },
         currencies: network.coinLookup.map(({ viewDenom }) => configToKeplrCoin(viewDenom)),
-        feeCurrencies: network.coinLookup.map(({ viewDenom }) => configToKeplrCoin(viewDenom)),
+        feeCurrencies: network.coinLookup.map(({ viewDenom }) => ({
+          ...configToKeplrCoin(viewDenom),
+          gasPriceStep: {
+            low: 1,
+            average: 10,
+            high: 1000,
+          },
+        })),
         coinType: 118,
-        gasPriceStep: {
-          low: 1,
-          average: 10,
-          high: 1000,
-        },
         features: [
           'stargate',
           'ibc-transfer',
