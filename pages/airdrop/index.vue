0ba4f5ed8555b33c10e48236eb0623ab67ccfa17
WeiJun0827 <weijunhung@outlook.com>
2022-10-15 14:53:59 +0800

    🔥 Keep only redirect function for `/airdrop` and `/airdrop/check`

--- pages/airdrop/index.vue
+++ pages/airdrop/index.vue
@@ -1,47 +1,7 @@
-<template>
-  <div
-    :class="[
-      'flex',
-      'flex-col',
-      'w-full',
-      'items-center',
-      'justify-center',
-      'px-[12px]',
-      'md:px-0',
-    ]"
-  >
-    <img
-      :class="[
-        'hidden',
-        'my-[32px]',
-        'w-[300px]',
-        'lg:block',
-      ]"
-      src="~/assets/images/title_Claim.png"
-    />
-    <Label
-      :class="[
-        'my-[24px]',
-        'font-extrabold',
-        'text-like-green',
-        'lg:hidden',
-        'text-center',
-      ]"
-      :text="$t('AirDrop.label.checker')"
-      preset="h2"
-      align="center"
-    />
-    <AirdropLogin />
-  </div>
-</template>
 <script lang="ts">
-import { Vue, Component, Watch } from 'vue-property-decorator'
-import { namespace } from 'vuex-class'
-
+import { Vue, Component } from 'vue-property-decorator'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { MetaInfo } from 'vue-meta'
-
-const signerModule = namespace('signer')
 
 @Component({
   head() {
@@ -73,17 +33,5 @@ const signerModule = namespace('signer')
   },
 })
 export default class AirdropPage extends Vue {
-  @signerModule.Getter('getAddress') walletAddress!: string
-
-  mounted() {
-    this.goClaimPage()
-  }
-
-  @Watch('walletAddress')
-  goClaimPage() {
-    if (this.walletAddress) {
-      this.$router.push(this.localeLocation({ name: 'airdrop-claim' })!)
-    }
-  }
 }
 </script>
