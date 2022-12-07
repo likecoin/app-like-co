11efb4bdc2b6a408a5b864bd1138b3f782ef8f84
AuroraHuang22 <aurora90376@gmail.com>
2022-12-07 22:06:29 +0800

    🎨 Check signing method before creating classId

--- pages/nft/iscn/_iscnId.vue
+++ pages/nft/iscn/_iscnId.vue
@@ -416,15 +416,15 @@ export default class NFTTestMintPage extends Vue {
         logTrackerEvent(this, 'IscnMintNFT', 'doActionNFTError', ErrorType.INSUFFICIENT_BALANCE, 1);
         throw new Error(ErrorType.INSUFFICIENT_BALANCE)
       }
+      if (this.isUsingLikerLandApp) {
+        this.errorMessage = this.$t('IscnRegisterForm.error.walletConnect') as string
+        throw new Error(ErrorType.USE_WALLET_CONNECT)
+      }
       /* eslint-disable no-fallthrough */
       switch (this.state) {
         case 'create': {
           this.isLoading = true
           const isAllowed = IS_TESTNET || await this.checkIsWhitelisted();
-          if (this.isUsingLikerLandApp) {
-            this.errorMessage = this.$t('IscnRegisterForm.error.walletConnect') as string
-            throw new Error(ErrorType.USE_WALLET_CONNECT)
-          }
           if (!isAllowed) {
             logTrackerEvent(this, 'IscnMintNFT', 'CreateNFTError', ErrorType.USER_NOT_WHITELISTED, 1);
             this.toggleSnackbar(ErrorType.USER_NOT_WHITELISTED)
