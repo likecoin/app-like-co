b5af87b5239b7c3a9a36092188f3b017369cafda
William Chong <me@williamchong.cloud>
2023-02-02 14:53:32 +0800

    💄 Use new mint icon for index CTA

---
+++ components/icon/MintBold.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="20"
+    height="20"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+  >
+    <path
+      d="m15.55,11.9l3.19,1.9-8.75,5.2L1.25,13.8l3.19-1.9,5.55,3.3,5.55-3.3Zm-5.55-.49l-5.55-3.3-3.19,1.9,8.75,5.2,8.75-5.2-3.19-1.9-5.55,3.3Zm8.75-5.2L10,1,1.25,6.2l8.75,5.2,8.75-5.2Z"
+      fill="none"
+      fill-rule="evenodd"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1.8"/>
+  </svg>
+</template>
