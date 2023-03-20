b79245fe2186b663a602fcb397ecab84a755afbe
AuroraHuang22 <aurora90376@gmail.com>
2023-03-20 21:11:55 +0800

    💄 <MintPageContainer> Add a block to require providing email

--- components/MintPageContainer.vue
+++ components/MintPageContainer.vue
@@ -11,6 +11,39 @@
       'lg:p-[16px]',
     ]"
   >
+    <div
+      :class="[
+        'flex',
+        'items-start',
+        'justify-start',
+        'gap-[8px]',
+        'px-[12px]',
+        'py-[8px]',
+        'bg-[#f1fbff]',
+        'border-[1px]',
+        'border-[#caedff]',
+        'rounded-[12px]',
+        'mb-[24px]',
+        'w-full',
+        'max-w-[600px]',
+      ]"
+    >
+      <IconAttention class="flex-shrink-0 text-[#a9d3e2]" />
+      <div class="flex flex-col">
+        <Label preset="p6" class="whitespace-pre-line">
+          <i18n path="NFTPortal.label.input.email" tag="div">
+            <template #link>
+              <a
+                v-t="$t('NFTPortal.label.input.link')"
+                :class="['underline', 'text-like-green']"
+                href="https://forms.gle/dwhq6Dny3DnDUEzg9"
+                target="_blank"
+              />
+            </template>
+          </i18n>
+        </Label>
+      </div>
+    </div>
     <slot />
     <div class="flex flex-col max-w-[600px]">
       <div class="mt-[-20px]">
