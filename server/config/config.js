a28a88e896bca3cdbb037188a59c0d7fcef7d723
William Chong <me@williamchong.cloud>
2023-09-16 02:19:16 +0800

    🔥 Remove whitelist server API, use api.like.co arweave

--- server/config/config.js
+++ server/config/config.js
@@ -1,21 +1,7 @@
-const { IS_TESTNET } = process.env;
-
 const config = {};
-
-config.IPFS_ENDPOINT = 'https://ipfs.io:5001/api/v0';
-config.REPLICA_IPFS_ENDPOINTS = [];
-config.WEB3_STORAGE_API_TOKEN = '';
-
-config.COSMOS_RPC_ENDPOINT = 'https://node.testnet.like.co/rpc/';
-config.COSMOS_DENOM = IS_TESTNET ? 'nanoekil' : 'nanolike';
-config.LIKE_TARGET_ADDRESS = 'like1rclg677y2jqt8x4ylj0kjlqjjmnn6w63uflpgr';
 
 config.NUMBERS_PROTOCOL_BASE_URL = '';
 config.NUMBERS_PROTOCOL_API_KEY = '';
 config.NUMBERS_PROTOCOL_TOKEN = '';
-
-config.NFT_WHITELIST_ADDRESSES = [];
-
-config.ARWEAVE_UPLOAD_TRY_LIMIT = 5;
 
 module.exports = config;
