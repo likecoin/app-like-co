d1a4c0362ac01c5c33ec347af490dfcf609b84e5
AuroraHuang22 <aurora90376@gmail.com>
2023-12-29 00:51:31 +0800

    💄 Update nftUrl button style

--- pages/view/_iscnId/index.vue
+++ pages/view/_iscnId/index.vue
@@ -112,9 +112,8 @@
         :to="localeLocation({ name: 'nft-iscn-iscnId', params: { iscnId: iscnId }, query: mintQueries })"
         :text="$t('NFTPortal.button.mint')"
       />
-      <div class="flex justify-center items-center p-[4px] rounded-[12px] border-like-cyan-light border-[2px]">
+      <div v-if="!isShowMintButton" class="flex justify-center items-center p-[4px] rounded-[12px] border-like-cyan-light border-[2px]">
         <Button
-          v-if="!isShowMintButton"
           preset="tertiary"
           :text="$t('NFTPortal.button.check.nft')"
           :href="likerlandNftUrl"
